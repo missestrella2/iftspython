2eb8917eb11fcee9da76f8d0de56c4a321ae9b2a
Cyn <amandaodoo@gmail.com>
2023-05-27 21:50:27 -0300

    actualizacion

--- proyecto/proyectocaja.py
+++ proyecto/proyectocaja.py
@@ -1,8 +1,10 @@
+##PROBADNO COSAS PARA EL FUTURO PROYECTO
+
 #diccionario de productos
 stock={
         1:{
             "producto":"pan lactal 200g",
-            "precio":"34.5",
+            "precio":"45",
             },
         2:{
             "producto":"tomate por unidad",
@@ -17,15 +19,48 @@ stock={
 acumulador=0
 
 #Ingresar el codigo de un producto y la cantidad y que me diga el subtotal de ese producto
-codigo = int(input("Ingrese codigo"))
+#RECORDAR AGREGAR EN CADA INPUT LA VALIDACION1!
+
+codigo = int(input("Ingrese codigo")) 
 cantidad = int(input("ingrese cantidad"))
-preciounitario = stock[codigo]["precio"]
+preciounitario = stock[codigo]["precio"] #asi consulto el elemento de un subdiccionario usando las key
 preciosubtotal = float(preciounitario) * float(cantidad)
 subtotalproducto = print("producto: {}, precio: {}, cant: {}, subtotal: {}".format(stock[codigo]["producto"],stock[codigo]["precio"],cantidad,preciosubtotal))
 acumulador = acumulador+preciosubtotal
 subtotaldetodo=print("Subtotal de la cuenta: {}".format(acumulador))
 
+# recordar crear un diccionario que va a ser el ticket
+# aca hacer que guarde el ultimo producto como un diccionario dentro del diccionario ticket
+# cada elemento del diccionario ticket va a ser una LINEA que contiene precio, cantidad, nombre del prod etc
+
 #aca habria que decidir la condicion para salir del bucle y totalizar la cuenta
 
 #total de la cuenta (iria fuera del bucle de pedir producto por producto)
+#aca hay que hacer esto: 
+# 1 que imprima el ticket con todo bien alineado con format
+# 2 que recorra el diccionario ticket y sus elementos LINEA 1, LINEA 2, LINEA 3
+
 print("Total: {}".format(acumulador))
+
+#ejemplo de internet para usar format en la impresion del ticket
+#:>10d   Entero alineado a la derecha en un campo de 10 caracteres
+#:<10d   Entero alineado a la izquierda en un campo de 10 caracteres
+#:^10d   Entero centrado en un campo de 10 caracteres
+#:0.2f   Flotante con dos dígitos de precisión
+
+print("Ejemplo sacado de internet de texto alineado")
+
+for i in range (6):
+    nombre = 'Naranja'
+    cajones = 100
+    precio = 91.1
+    print(f'{nombre:>10s} {cajones:>10d} {precio:>10.2f}')
+
+
+## cosas a revisar y a tener en cuenta,preguntar a la profe
+## hay que dar de alta productos?
+## hay que poner medios de pago diferentes?
+## el programa finaliza cuando se termina de facturar o queda listo para poder facturar despues?
+## tiene que llevar fecha, direccion del negocio, etc? o algun otro dato adicional? 
+## se puede anular tickets, reimprimirlos, etc?
+## los tickets tambien hay que ir guardandolos? 
